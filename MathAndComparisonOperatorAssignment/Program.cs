c9b46e63d5aad58b8c8bacb5af79c84233200114
SarahIrons <sarahirons11@gmail.com>
2024-01-25 16:55:39 -0500

    Add project files.

---
+++ MathAndComparisonOperatorAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class PayRate
+{
+    public float hourlyRate { get; set; }
+    public float hours { get; set; }
+    public float income => hours * hourlyRate * 52;
+
+}
+namespace MathAndComparisonOperatorAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var payRateA = new PayRate();
+            var payRateB = new PayRate();
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1:");
+            Console.WriteLine("Hourly Rate:");
+            payRateA.hourlyRate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week:");
+            payRateA.hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Annual income:$" + payRateA.income);
+            Console.WriteLine("Person 2:");
+            Console.WriteLine("Hourly Rate:");
+            payRateB.hourlyRate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worked per week:");
+            payRateB.hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Annual income:$ " + payRateB.income);
+            Console.WriteLine("Does  Person 1 make more than Person 2?");
+            Console.Write(payRateA.income > payRateB.income);
+        }
+    }
+}
